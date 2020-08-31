80f6e0f6dbdac206fa7c1453417bb6cacb2fb91e
Kuanyshkhan <64012843+Kuanyshhan@users.noreply.github.com>
2020-08-31 21:38:00 +0600

    Приведение типов, строка и число

--- 01_types.js
+++ 01_types.js
@@ -10,15 +10,29 @@
 // console.log(typeof Symbol('JS'))
 
 
-let language = 'Javascript'
-if(language){
-    console.log('The best language is', language)
-}
+// let language = 'Javascript'
+// if(language){
+//     console.log('The best language is', language)
+// }
 // приведение типов
 // 0, false, null, undefined, NaN, ''
-console.log(Boolean(0))
-console.log(Boolean(1))
-console.log(Boolean(''))
-console.log(Boolean(' '))
-console.log(Boolean(null))
-console.log(Boolean(undefined))
+// console.log(Boolean(0))
+// console.log(Boolean(1))
+// console.log(Boolean(''))
+// console.log(Boolean(' '))
+// console.log(Boolean(null))
+// console.log(Boolean(undefined))
+
+
+//  строка и число
+console.log(1 + '2') // string
+console.log(1 + 2 + '3') 
+console.log('' - 5 + 16)
+console.log('6' - 5) //number
+console.log('1' + 5 - 2)
+console.log('8' * '3') //number
+console.log('8px' - '3')
+console.log('px' + 1 + 5)
+console.log(1 + 2 + 'px')
+console.log(null + 2)
+console.log(undefined + 6)
