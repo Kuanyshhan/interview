950c08299e0a9d430cdc2bc1ff55600278928a2e
Kuanyshkhan <64012843+Kuanyshhan@users.noreply.github.com>
2020-08-31 21:26:29 +0600

    приведение типов, с помощью Boolean

--- 01_types.js
+++ 01_types.js
@@ -1,10 +1,24 @@
 // null, undefined, string, number, boolean, object, symbol
 
-console.log(typeof null)
-console.log(typeof undefined)
-console.log(typeof 'Javascript')
-console.log(typeof 0)
-console.log(typeof false)
-console.log(typeof Math)
-console.log(typeof function(){})
-console.log(typeof Symbol('JS'))
+// console.log(typeof null)
+// console.log(typeof undefined)
+// console.log(typeof 'Javascript')
+// console.log(typeof 0)
+// console.log(typeof false)
+// console.log(typeof Math)
+// console.log(typeof function(){})
+// console.log(typeof Symbol('JS'))
+
+
+let language = 'Javascript'
+if(language){
+    console.log('The best language is', language)
+}
+// приведение типов
+// 0, false, null, undefined, NaN, ''
+console.log(Boolean(0))
+console.log(Boolean(1))
+console.log(Boolean(''))
+console.log(Boolean(' '))
+console.log(Boolean(null))
+console.log(Boolean(undefined))
