6540a5d593361b10b28984e2a886895f0999150e
Kuanyshkhan <64012843+Kuanyshhan@users.noreply.github.com>
2020-08-31 21:14:53 +0600

    Определение типы данных с помощью typeof

---
+++ 01_types.js
@@ -0,0 +1,10 @@
+// null, undefined, string, number, boolean, object, symbol
+
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof 'Javascript')
+console.log(typeof 0)
+console.log(typeof false)
+console.log(typeof Math)
+console.log(typeof function(){})
+console.log(typeof Symbol('JS'))
