79bd5e22833eb36645f4813ae6b9886dfb0617d7
Kuanyshkhan <64012843+Kuanyshhan@users.noreply.github.com>
2020-09-02 11:17:39 +0600

    Если проверяем с массивами, то работает иначе

--- 02_values.js
+++ 02_values.js
@@ -1,5 +1,14 @@
-let a = 5
+// let a = 5
+// let b = a
+// b++
+// console.log('a=', a)
+// console.log('b=', b)
+
+let a = [1, 2, 3]
 let b = a
-b++
-console.log('a=', a)
-console.log('b=', b)
+b.push(4)
+console.log('a', a)
+console.log('b', b)
+let c = [1, 2, 3, 4]
+console.log(a == c)
+console.log(b == c)
