c8ce7b1597f13118c0f6562ba42bed3f4413f0b1
Kuanyshkhan <64012843+Kuanyshhan@users.noreply.github.com>
2020-09-02 19:24:07 +0600

    Создали 03_scope.js

--- 02_values.js
+++ 02_values.js
@@ -4,11 +4,11 @@
 // console.log('a=', a)
 // console.log('b=', b)
 
-let a = [1, 2, 3]
-let b = a
-b.push(4)
-console.log('a', a)
-console.log('b', b)
-let c = [1, 2, 3, 4]
-console.log(a == c)
-console.log(b == c)
+// let a = [1, 2, 3]
+// let b = a
+// b.push(4)
+// console.log('a', a)
+// console.log('b', b)
+// let c = [1, 2, 3, 4]
+// console.log(a == c)
+// console.log(b == c)
