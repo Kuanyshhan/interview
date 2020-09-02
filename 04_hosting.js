dcea16e82fd4c43779eaf147e56640b15475642c
Kuanyshkhan <64012843+Kuanyshhan@users.noreply.github.com>
2020-09-02 20:12:30 +0600

    Function Expression and Function Declaration

--- 04_hosting.js
+++ 04_hosting.js
@@ -3,7 +3,21 @@
 //     return a + b
 // }
 
-var i
-console.log(i)
-i = 3
-console.log(i)
+// var i
+// console.log(i)
+// i = 3
+// console.log(i)
+
+// console.log(sum)
+// var sum = 11
+// console.log
+
+// Function Expression & Function Declaration
+console.log(square(19));
+function square(num) {
+    return num ** 2
+}
+const square1 = function(num1) {
+    return num1 ** 2
+}
+console.log(square1(17))
