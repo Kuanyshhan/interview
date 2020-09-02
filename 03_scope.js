3235d6887b79dc329bba2c4c626681ec3afe0897
Kuanyshkhan <64012843+Kuanyshhan@users.noreply.github.com>
2020-09-02 19:51:21 +0600

    Создали 04_hosting.js

--- 03_scope.js
+++ 03_scope.js
@@ -1,17 +1,17 @@
-function funcA() {
-    let a = 1
+// function funcA() {
+//     let a = 1
 
-    function funcB() {
-        let b = 2
-        
-        function funcC() {
-            let c = 3
-            console.log('funcC:', a, b, c)
-        }
-        console.log('funcB:', a, b)
-        funcC()
-    }
-    console.log('funcA:', a)
-    funcB()
-}
-funcA()
+//     function funcB() {
+//         let b = 2
+
+//         function funcC() {
+//             let c = 3
+//             console.log('funcC:', a, b, c)
+//         }
+//         console.log('funcB:', a, b)
+//         funcC()
+//     }
+//     console.log('funcA:', a)
+//     funcB()
+// }
+// funcA()
