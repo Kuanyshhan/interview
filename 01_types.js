0353580366c91b1eebae3847839a112a0e87bae4
Kuanyshkhan <64012843+Kuanyshhan@users.noreply.github.com>
2020-09-02 11:07:42 +0600

    Создали 02_values.js

--- 01_types.js
+++ 01_types.js
@@ -48,12 +48,12 @@
 
 // =================
 
-console.log(false == '')
-console.log(false == [])
-console.log(false == {})
-console.log('' == 0)
-console.log('' == [])
-console.log('' == {})
-console.log(0 == [])
-console.log(0 == {})
-console.log(0 == null)
+// console.log(false == '')
+// console.log(false == [])
+// console.log(false == {})
+// console.log('' == 0)
+// console.log('' == [])
+// console.log('' == {})
+// console.log(0 == [])
+// console.log(0 == {})
+// console.log(0 == null)
