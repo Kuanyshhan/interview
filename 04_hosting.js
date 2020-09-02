c4823b90e84eb2cba679be4404653e297bc21bba
Kuanyshkhan <64012843+Kuanyshhan@users.noreply.github.com>
2020-09-02 19:57:34 +0600

    hosting начале с переменной var, будет undefined

--- 04_hosting.js
+++ 04_hosting.js
@@ -1,4 +1,9 @@
-console.log(sum(6, 7))
-function sum(a, b){
-    return a + b
-}
+// console.log(sum(6, 7))
+// function sum(a, b){
+//     return a + b
+// }
+
+var i
+console.log(i)
+i = 3
+console.log(i)
